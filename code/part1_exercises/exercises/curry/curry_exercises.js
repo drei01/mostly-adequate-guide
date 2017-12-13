182200385e49b0522ec94b390f7a5c397f39bb7f
Matthew Reid <mreid@small-improvements.com>
2017-12-13 17:45:26 +0100

    Exercises part 1

--- code/part1_exercises/exercises/curry/curry_exercises.js
+++ code/part1_exercises/exercises/curry/curry_exercises.js
@@ -1,64 +1,67 @@
 require('../../support');
 var _ = require('ramda');
 
-
 // Exercise 1
 //==============
 // Refactor to remove all arguments by partially applying the function
 
-var words = function(str) {
-  return split(' ', str);
-};
+var words = _.curry(function(str) {
+    return split(' ', str);
+});
 
 // Exercise 1a
 //==============
 // Use map to make a new words fn that works on an array of strings.
 
-var sentences = undefined;
-
+var sentences = _.map(words);
 
 // Exercise 2
 //==============
 // Refactor to remove all arguments by partially applying the functions
 
-var filterQs = function(xs) {
-  return filter(function(x){ return match(/q/i, x);  }, xs);
-};
+var matchQ = _.curry(function(x) {
+    return match(/q/i, x);
+});
 
+var filterQs = _.curry(function(xs) {
+    return filter(matchQ, xs);
+});
 
 // Exercise 3
 //==============
 // Use the helper function _keepHighest to refactor max to not reference any arguments
 
 // LEAVE BE:
-var _keepHighest = function(x,y){ return x >= y ? x : y; };
+var _keepHighest = _.curry(function(x, y) {
+    return x >= y ? x : y;
+});
 
 // REFACTOR THIS ONE:
-var max = function(xs) {
-  return reduce(function(acc, x){
-    return _keepHighest(acc, x);
-  }, -Infinity, xs);
-};
+var max = _.curry(function(xs) {
+    return reduce(_keepHighest, -Infinity, xs);
+});
 
-  
 // Bonus 1:
 // ============
 // wrap array's slice to be functional and curried.
 // //[1,2,3].slice(0, 2)
-var slice = undefined;
-
+var slice = _.curry(function(from, to, array) {
+    return array.slice(from, to);
+});
 
 // Bonus 2:
 // ============
 // Use slice to define a function "take" that returns n elements from the beginning of an array. Make it curried.
 // For ['a', 'b', 'c'] with n=2 it should return ['a', 'b']
-var take = undefined;
+var take = _.curry(function(n, array) {
+    return slice(0)(n)(array);
+});
 
-
-module.exports = { words: words,
-                   sentences: sentences,
-                   filterQs: filterQs,
-                   max: max,
-                   slice: slice,
-                   take: take
-                 };
+module.exports = {
+    words: words,
+    sentences: sentences,
+    filterQs: filterQs,
+    max: max,
+    slice: slice,
+    take: take,
+};
